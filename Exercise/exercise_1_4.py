3a92c402c5bc758467dfdf03ea304b38aa1c7c8e
= <=>
2021-05-28 14:33:38 +0200

    first commit

---
+++ Exercise/exercise_1_4.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+v0 = 5
+g = 9.81
+t = np.linspace(1, 3, 3)
+
+y = t**3
+
+plt.plot(t, y)
+plt.xlabel('Length of side')
+plt.ylabel('Volume')
+plt.show()
